a2e991a9c51d9492c714d88725e6e7abf01a4e69
Thierry <thierry.tj007@gmail.com>
2023-11-02 17:58:42 -0400

    Error page added, App page formatted without authentication

---
+++ market/client/src/App.jsx
@@ -0,0 +1,20 @@
+import {Route, Routes} from "react-router-dom";
+import Auction from "./pages/Auction"
+import Home from "./pages/Home"
+import MyAuction from "./pages/MyAuction"
+import Winnings from "./pages/Winnings"
+import Error from "./pages/Error"
+
+const App = () => {
+    return (
+        <div className = 'App'>
+            <Routes>
+                <Route path ='*' element={<Error />}/>
+                <Route path ='/' element={<Home />}/>
+                <Route path ='Auction' element={<Auction />}/>
+                <Route path ='/MyAuction' element={<MyAuction />}/>
+                <Route path ='/Winnings' element={<Winnings />}/>
+            </Routes>
+        </div>
+    )
+}
