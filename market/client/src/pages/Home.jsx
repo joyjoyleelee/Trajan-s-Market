805cfeeea3ec617a341b8ac2370f824d41facad8
Thierry <thierry.tj007@gmail.com>
2023-11-02 19:42:12 -0400

    adding backend folder

--- market/client/src/pages/Home.jsx
+++ market/client/src/pages/Home.jsx
@@ -1,11 +1,27 @@
 import { Button, Grid, Container, Modal } from '@mui/material';
 import React, { useState } from 'react';
+import LoginModal from '../../components/home/LoginModal';
+import SignupModal from '../../components/home/SignupModal';
 
 const Home = () => {
   const [logOpen, setLog] = useState(false)
   const [regOpen, setReg] = useState(false)
 
+  const handleLogOpen = () =>{
+    setLog(true)
+  }
 
+  const handleLogClose = () =>{
+    setLog(false)
+  }
+
+  const handleRegOpen = () =>{
+    setReg(true)
+  }
+
+  const handleRegClose = () =>{
+    setReg(false)
+  }
 
   return (
       <h1>404 Home Found</h1>
