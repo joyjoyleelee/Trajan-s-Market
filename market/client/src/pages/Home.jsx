53aaf83379888c9d08fb5749ad1979b48959b7c0
Thierry <thierry.tj007@gmail.com>
2023-11-02 19:22:34 -0400

    Added api folder, added code for Home.jsx

--- market/client/src/pages/Home.jsx
+++ market/client/src/pages/Home.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import { Button, Grid, Container, Modal } from '@mui/material';
+import React, { useState } from 'react';
 
 const Home = () => {
+  const [logOpen, setLog] = useState(false)
+  const [regOpen, setReg] = useState(false)
+
+
+
   return (
-    <h1>404 Home Found</h1>
+      <h1>404 Home Found</h1>
   )
 }
 
