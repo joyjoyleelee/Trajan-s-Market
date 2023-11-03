4d7fc91c09ee19393b3c8b6a089eec406f43dcfe
Thierry <thierry.tj007@gmail.com>
2023-11-02 20:21:27 -0400

    finally

--- market/client/src/index.jsx
+++ market/client/src/index.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom'; // You can use BrowserRouter or another router type
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import {HashRouter} from "react-router-dom";
 
-import App from './App';
-
-ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
-  document.getElementById('root')
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<HashRouter>
+			<App />
+		</HashRouter>
+	</React.StrictMode>
 );
--- market/client/src/pages/Home.jsx
+++ market/client/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import { Button, Grid, Container, Modal } from '@mui/material';
 import React, { useState } from 'react';
-import LoginModal from '../../components/home/LoginModal';
-import SignupModal from '../../components/home/SignupModal';
+// import LoginModal from '../../components/home/LoginModal';
+// import SignupModal from '../../components/home/SignupModal';
 
 const Home = () => {
   const [logOpen, setLog] = useState(false)
@@ -24,7 +24,38 @@ const Home = () => {
   }
 
   return (
-      <h1>404 Home Found</h1>
+       <Container maxWidth='auto'
+             style={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                backgroundColor: 'lightgrey',
+                minHeight: 'auto',
+                padding: '10px',
+             }}>
+         <Grid container>
+            <Grid item sx={{ margin: 'auto' }}>
+              <Button
+                onClick={handleLogOpen}
+                variant='contained'
+                color='primary'
+                sx={{ width: '150px', height: '50px'}}
+              >
+                  Login
+              </Button>
+            </Grid>
+            <Grid item sx={{ margin: 'auto' }}>
+              <Button
+                onClick={handleRegOpen}
+                variant='contained'
+                color='primary'
+                sx={{ width: '150px', height: '50px'}}
+              >
+                  Register
+              </Button>
+            </Grid>
+         </Grid>
+       </Container>
   )
 }
 
