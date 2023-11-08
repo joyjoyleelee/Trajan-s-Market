5f306dbf850cdc2cc0d4dd3b73fdc0ec57236c58
Thierry <thierry.tj007@gmail.com>
2023-11-08 02:06:19 -0500

    implemented static listing, configured MyAuction

--- market/client/src/pages/Auction.jsx
+++ market/client/src/pages/Auction.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Listing from '../components/auction/Listing';
 import Navbar from '../components/Navbar'
 
+
 const Auction = () => {
 
     console.log("Auction page reached");
--- market/client/src/pages/MyAuction.jsx
+++ market/client/src/pages/MyAuction.jsx
@@ -1,15 +1,36 @@
-import React, { useState } from 'react';
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemText from "@mui/material/ListItemText";
+import React from 'react';
 import Navbar from '../components/Navbar'
+import StaticListing from "../components/StaticListing";
 
 const MyAuction = () => {
+
+    const tempPropsList = [
+        {
+            image: "favicon.ico",
+            title: "Child Slave",
+            description: "straight from Mongolia",
+            price: 1000,
+            end: 202311302359, // YYYYMMDD, time (military)
+            bidder: "none",
+        },
+        {
+            image: "favicon.ico",
+            title: "Adult Slave",
+            description: "straight from Czech Republic",
+            price: 3000,
+            end: 202311302359, // YYYYMMDD, time (military)
+            bidder: "none",
+        },
+    ];
+
     return (
         <>
             <Navbar></Navbar>
-            <h1>404 MyAuction Not Implemented</h1>
+            <div style={{ display: 'flex', gap: '20px', marginTop: '20px' }}>
+                {tempPropsList.map((tempProp, index) => (
+                    <Listing key={index} {...tempProp} />
+                ))}
+            </div>
         </>
     )
 }
