34fb2683243e7e2de19d92b05db2c7712a5f9f62
stevenwang <swang57@buffalo.edu>
2023-11-09 19:56:36 -0500

    connecting back and front end for login v2

--- market/backend/server.py
+++ market/backend/server.py
@@ -5,6 +5,8 @@ import bcrypt
 import hashlib
 import json
 import html
+#import os
+
 from bson.binary import Binary
 import base64
 
@@ -28,21 +30,23 @@ xsrf_token_collection = db["xsrf"]
 #Set up the home page ----------------------------------------------------------------------------------------------------------------------------
 @app.route("/") #index.html
 def home():
+    #index_html
+    #app = Flask(__name__, template_folder='../../client/public')
     response = make_response(render_template("index.html"), 200)
     response.headers["X-Content-Type-Options"] = "nosniff"
     return response
 
-@app.route("/<path:file>")
-def pathRoute(file):
-    response = make_response(send_from_directory("static", file), 200)
-    response.headers["X-Content-Type-Options"] = "nosniff"
-    return response
+# @app.route("/<path:file>")
+# def pathRoute(file):
+#     response = make_response(send_from_directory("static", file), 200)
+#     response.headers["X-Content-Type-Options"] = "nosniff"
+#     return response
 
 #Set up the registration form ---------------------------------------------------------------------------------------------------------------------
-@app.route("/register", methods =['GET', 'POST'])
+@app.route("/register", methods =['POST'])
 
 #Set up the login form-----------------------------------------------------------------------------------------------------------------------------
-@app.route("/login", methods =['GET', 'POST'])
+@app.route("/login", methods =['POST'])
 
 @app.route("/chat-message", methods = ["POST"])
 def makePost():
