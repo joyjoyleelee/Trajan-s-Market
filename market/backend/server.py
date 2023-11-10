3d3403c2c8fa8d90d40b2be718bc2a534b65fc21
Thierry <thierry.tj007@gmail.com>
2023-11-09 21:04:07 -0500

    beginning login integration

---
+++ market/backend/server.py
@@ -0,0 +1,62 @@
+from flask import Flask, render_template, make_response, url_for, request, send_from_directory, redirect
+from pymongo import MongoClient
+import json
+import html
+
+
+app = Flask(__name__) #setting this equal to the file name (web.py)
+
+#Establish the mongo database
+mongo_client = MongoClient('localhost')
+db = mongo_client["colosseum"]
+chat_collection = db["chat"]
+user_collection = db["users"]
+auth_token_collection = db["auth_tokens"]
+xsrf_token_collection = db["xsrf"]
+
+# Delete collection records. --- ALERT """ FOR TESTING ONLY MAKE SURE TO REMOVE
+# chat_collection.delete_many({})
+# user_collection.delete_many({})
+# auth_token_collection.delete_many({})
+# xsrf_token_collection.delete_many({})
+# MAKE SURE YOU REMOVE THE LINES ABOVE.
+
+#Set up the home page ----------------------------------------------------------------------------------------------------------------------------
+@app.route("/") #index.html
+def home():
+    #index_html
+    #app = Flask(__name__, template_folder='../../client/public')
+    response = make_response(render_template("index.html"), 200)
+    response.headers["X-Content-Type-Options"] = "nosniff"
+    return response
+
+# @app.route("/<path:file>")
+# def pathRoute(file):
+#     response = make_response(send_from_directory("static", file), 200)
+#     response.headers["X-Content-Type-Options"] = "nosniff"
+#     return response
+
+#Set up the registration form ---------------------------------------------------------------------------------------------------------------------
+@app.route("/register", methods =['POST']):
+
+
+#Set up the login form-----------------------------------------------------------------------------------------------------------------------------
+@app.route("/login", methods =['POST']):
+
+
+
+def postsFromDB():
+    # Function returns a list of all Posts
+    ret_list = []
+    all_posts = chat_collection.find({})
+    for p in all_posts:
+        #Escape HTML in the posts
+        p["title"] = html.escape(p["title"])
+        p["message"] = html.escape(p["message"])
+        p["username"] = html.escape(p["username"])
+        ret_list.append(p)
+    return ret_list
+
+
+
+app.run(host = "0.0.0.0", port = 8080)
