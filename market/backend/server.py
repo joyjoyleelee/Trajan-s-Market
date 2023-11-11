9116a5f0fb6d7c4641d5386ab37624a7fcf8a3f8
stevenwang <swang57@buffalo.edu>
2023-11-11 00:10:10 -0500

    add auth to login but it doesn't seem to work since the requests are different from project 2s requests

--- market/backend/server.py
+++ market/backend/server.py
@@ -41,6 +41,7 @@ def home():
 
     response = make_response(render_template("index.html"), 200)
     response.headers["X-Content-Type-Options"] = "nosniff"
+    print (response)
     return response
 
 
@@ -52,12 +53,12 @@ def pathRoute(file):
 
 #Set up the registration form ---------------------------------------------------------------------------------------------------------------------
 #Set up the login form-----------------------------------------------------------------------------------------------------------------------------
-@app.route("/login", methods =['GET', 'POST'])
+#@app.route("/login", methods =['GET', 'POST'])
 
 @app.route("/chat-message", methods = ["POST"])
 def makePost():
     post = request.data.decode() # Request post as a string
-    post = json.loads(ßpost) # Load Post as a Dictionary in format title: message: xsrf:
+    post = json.loads(post) # Load Post as a Dictionary in format title: message: xsrf:
     username = "Guest"
 
     # Verify user via authentication token
@@ -96,16 +97,83 @@ def makePost():
 
 #Set up the registration form ---------------------------------------------------------------------------------------------------------------------
 @app.route("/register", methods =['POST'])
+
+def process_register():
+    # print(request.form.get('username_reg')) # -> gets the username input
+    # print(request.get_data()) # -> b'username_reg=hi&password_reg=here'
+    #if (request.method == 'POST'):
+        # Check if the user does not exist yet -> valid
+        if (user_collection.find_one({"username": request.form['username_reg']}) == None):
+            # Store username and salted, hashed password in database
+            # print("this is a test")
+            #print(request)
+            salt = bcrypt.gensalt()
+            the_hash = bcrypt.hashpw(request.form['password_reg'].encode(), salt)
+            user_collection.insert_one({"username": request.form['username_reg'], "password": the_hash})
+
+            # Make response - USER DOES NOT EXIST -> GOOD
+            #response = make_response(render_template("index.html"), 200)
+            #response.headers["X-Content-Type-Options"] = "nosniff"
+        # If the user already exists, give an error
+        #else:
+            # Make response - USER ALREADY EXISTS -> NOT GOOD
+            #response = make_response("User already exists", 404)
+            #response.headers["X-Content-Type-Options"] = "nosniff"
+        return jsonify({"message": "User successfully added"})
+
+'''
 def process_register():
     print("Register path reached")
     data = request.json
     user_collection.insert_one(data)
     return jsonify({"message": "User successfully added"})
 
+'''
+
 
 
 #Set up the login form-----------------------------------------------------------------------------------------------------------------------------
 @app.route("/login", methods =['POST'])
+def login():
+    # print(request.get_data()) # -> b'username_login=hi&password_login=here'
+    # DB represents database
+    user_database = user_collection.find_one({"username": request.form['username_login']})
+    if (user_database == None):
+        # Make response - NO USERS EXIST YET -> NOT GOOD
+        response = make_response("Nothing in database", 404)
+        response.headers["X-Content-Type-Options"] = "nosniff"
+    else:
+        # Access the password associated with the username
+        database_password = user_database.get("password", b'none')
+        print(database_password)
+        # Access the password associated with what the user gave us
+        input_password = request.form['password_login'].encode()
+        print(input_password)
+        # If the user does not exist in the database, then database_password = b"none"
+        if (database_password == b'none'):
+            # Make response if USER DOES NOT EXIST
+            response = make_response("User does not exist", 404)
+            response.headers["X-Content-Type-Options"] = "nosniff"
+        # Compare if the passwords are the same - returns True or False
+        elif (bcrypt.checkpw(input_password, database_password)):
+            auth_token = token_urlsafe(13)  # creates unique token, the 13 is the entropy
+
+            # Make response if PASSWORDS MATCH
+            response = make_response(render_template("index.html"), 200)
+            response.headers["X-Content-Type-Options"] = "nosniff"
+
+            # Set the authentication cookie and add to auth_token database named "auth_tokens"
+            auth_token_hashed = hashlib.sha256(auth_token.encode('utf-8')).digest()
+            response.set_cookie("auth_token", str(auth_token), max_age=3600, httponly=True)
+            response.set_cookie("cookie_name", request.form['username_login'])
+            auth_token_collection.insert_one(
+                {"username": request.form['username_login'], "auth_token": auth_token_hashed})
+
+        else:
+            # Make response if PASSWORDS DO NOT MATCH
+            response = make_response(render_template("index.html"), 404)
+            response.headers["X-Content-Type-Options"] = "nosniff"
+    return jsonify({"message": "User successfully logged in"})
 
 
 
