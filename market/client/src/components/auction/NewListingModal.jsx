ab56b72d9553b68713b07408c544e98c11e3b1ac
Thierry <thierry.tj007@gmail.com>
2023-11-12 12:49:30 -0500

    adding to new listing modal

--- market/client/src/components/auction/NewListingModal.jsx
+++ market/client/src/components/auction/NewListingModal.jsx
@@ -1,13 +1,12 @@
 import { TextField, Button, Container, Grid } from '@mui/material';
 import React, { useState } from 'react';
 
-const NewListingModal = () =>{
-    const [name, setName] = setState("")
-    const [descr, setDescr] = setState("")
-    const [price, setPrice] = setPrice("")
-    const [yearEnd, setYearEnd] = setState(0)
-    const [monthEnd, setMonthEnd] = setState(0)
-    const [dayEnd, setDayEnd] = setState(0)
+const NewListingModal = ({handleLogClose}) =>{
+    const [name, setName] = setState("");
+    const [descr, setDescr] = setState("");
+    const [price, setPrice] = setPrice(1.0);
+    const [endDate, setEndDate] = setState(null);
+    const [photo, setPhoto] = setState(null);
 
     const handleName = (e) => {
         setName(e.target.value);
@@ -18,22 +17,88 @@ const NewListingModal = () =>{
     };
 
     const handlePrice = (e) =>{
-        setPrice(e.target.value)
+        setPrice(e.target.value);
+    };
+
+    const handleDate = (date) =>{
+        setEndDate(date);
+    };
+
+    const sendData = () =>{
+    //     Send http
+        handleLogClose()
     }
 
-    const handleYear = (e) =>{
-        setYearEnd(e.target.value)
-    }
-
-    const handleMonth = (e) =>{
-        setMonthEnd(e.target.value)
-    }
-
-    const setDay = (e) =>{
-        setDayEnd(e.target.value)
-    }
-
-    return (<></>);
+    return (
+        <Container maxWidth="auto"
+                   style={{ display: 'flex',
+                       alignItems: 'center',
+                       justifyContent: 'center',
+                       backgroundColor: 'lightgrey',
+                       minHeight: 'auto',
+                       padding:'10px',}}>
+            <Grid container spacing={2}>
+            <Grid>
+            {/*  Photo  */}
+            </Grid>
+            <Grid item xs={12}>
+                <TextField
+                    label="Item Name"
+                    required
+                    type="text"
+                    variant="outlined"
+                    value={name}
+                    onChange={handleName}
+                />
+            </Grid>
+            <Grid item xs={12}>
+                <TextField
+                    label="Item Description"
+                    required
+                    type="text"
+                    variant="outlined"
+                    onChange={handleDescr}
+                    value={descr}
+                    multiline
+                    rows={4}
+                />
+            </Grid>
+            <Grid item xs={12}>
+                <TextField
+                    label="Price"
+                    required
+                    type="number"
+                    variant="outlined"
+                    onChange={handlePrice}
+                    value={price}
+                />
+            </Grid>
+            <Grid>
+            {/*    DATE*/}
+            </Grid>
+            <Grid item xs={6}>
+                <Button
+                    onClick={sendData}
+                    variant="contained"
+                    color="primary"
+                    sx={{ width: '150px', height: '50px'}}
+                >
+                    Register
+                </Button>
+            </Grid>
+            <Grid item xs={6}>
+                <Button
+                    onClick={handleLogClose}
+                    variant="contained"
+                    color="primary"
+                    sx={{ width: '150px', height: '50px'}}
+                >
+                    Cancel
+                </Button>
+            </Grid>
+        </Grid>
+    </Container>
+    );
 };
 
 export default NewListingModal
