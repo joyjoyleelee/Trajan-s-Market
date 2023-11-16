1bbc9ebe626a32cbc8b4ca2cfef58884a0ce4e95
Thierry <thierry.tj007@gmail.com>
2023-11-16 18:32:31 -0500

    Adding http requests for image upload and createListing, not fully complete, just setting up

---
+++ market/backend/static/createListing.js
@@ -0,0 +1,5 @@
+function submitForm() {
+        // Implement the logic for form submission here
+        // This is a placeholder function, and you should replace it with your actual submission logic
+        console.log('Form Submitted');
+    }
